9b3147f3a1172abb80a2a4214b2cc0eaf2109c60
nishithpatel1511 <nishithjpatel22@gmail.com>
2021-01-02 23:18:42 +0530

    Navbar Scrolling

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 var clk = 0;
       $(function () 
       {
+          //click on toggler
           $('#tg').click(function () {
           clk++;
           $('#tg').toggleClass('fa-times');
@@ -8,15 +9,18 @@ var clk = 0;
            $('div:not(#mySidepanel)').css("filter", "blur(0px)");
            document.body.style.backgroundColor = "white"; 
             //$('#mySidepanel').css({ 'width': 0 });
-            $('#mySidepanel').toggleClass('opensidepanel');
+            $('#mySidepanel').toggleClass('opensidepanel'); 
           }
           else {
             $('#mySidepanel').toggleClass('opensidepanel');
            $('div:not(#mySidepanel)').css("filter", "blur(4px)");
            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+           var a = ($('#mySidepanel').hasclass('sidepanel'));
+            alert(a);
           }
         });
-
+        
+        //click on the close btn
         $('.closebtn').click(function () {
           clk++;
           $('#tg').toggleClass('fa-times');
@@ -25,16 +29,18 @@ var clk = 0;
             $('#mySidepanel').toggleClass('opensidepanel');
           
          });
-        // $(document).click(function(e)
-        // {
-        //   if($(e.target).closest('#mySidepanel').length == 0)
-        //   {
-        //     clk++;
-        //     $('#tg').toggleClass('fa-times');
-        //     $('div:not(#mySidepanel)').css("filter", "blur(0px)");
-        //      document.body.style.backgroundColor = "white"; 
-        //     $('#mySidepanel').toggleClass('opensidepanel');
-        //   }
-        // });
+
+        //click outside automatic close
+        $(document).click(function(e)
+        {
+          if(($(e.target).closest('#mySidepanel').length == 0)&($('#mySidepanel').width() == 250))
+          {
+            clk++;
+            $('#tg').toggleClass('fa-times');
+            $('div:not(#mySidepanel)').css("filter", "blur(0px)");
+             document.body.style.backgroundColor = "white"; 
+            $('#mySidepanel').toggleClass('opensidepanel');
+          }
+        });
 
       });
