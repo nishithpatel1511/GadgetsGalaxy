778bab4c277920c042d0366d284e205109d1387d
nishithpatel1511 <nishithjpatel22@gmail.com>
2021-01-02 23:31:58 +0530

    Navbar Scrolling

--- js/nav.js
+++ js/nav.js
@@ -1,46 +1,43 @@
 var clk = 0;
-      $(function () 
-      {
-          //click on toggler
-          $('#tg').click(function () {
-          clk++;
-          $('#tg').toggleClass('fa-times');
-          if (clk % 2 == 0) {
-           $('div:not(#mySidepanel)').css("filter", "blur(0px)");
-           document.body.style.backgroundColor = "white"; 
-            //$('#mySidepanel').css({ 'width': 0 });
-            $('#mySidepanel').toggleClass('opensidepanel'); 
-          }
-          else {
-            $('#mySidepanel').toggleClass('opensidepanel');
-           $('div:not(#mySidepanel)').css("filter", "blur(4px)");
-           document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-           var a = ($('#mySidepanel').hasclass('sidepanel'));
-            alert(a);
-          }
-        });
-        
-        //click on the close btn
-        $('.closebtn').click(function () {
-          clk++;
-          $('#tg').toggleClass('fa-times');
-           $('div:not(#mySidepanel)').css("filter", "blur(0px)");
-            document.body.style.backgroundColor = "white"; 
-            $('#mySidepanel').toggleClass('opensidepanel');
-          
-         });
+$(function () {
+  $('#tg').click(function togglebtn() {
+    clk++;
+    $('#tg').toggleClass('fa-times');
+    if (clk % 2 == 0) {
+      $('div:not(#mySidepanel)').css("filter", "blur(0px)");
+      document.body.style.backgroundColor = "white";
+      //$('#mySidepanel').css({ 'width': 0 });
+      $('#mySidepanel').toggleClass('opensidepanel');
+    }
+    else {
+      $('#mySidepanel').toggleClass('opensidepanel');
+      $('div:not(#mySidepanel)').css("filter", "blur(4px)");
+      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    }
+  });
 
-        //click outside automatic close
-        $(document).click(function(e)
-        {
-          if(($(e.target).closest('#mySidepanel').length == 0)&($('#mySidepanel').width() == 250))
-          {
-            clk++;
-            $('#tg').toggleClass('fa-times');
-            $('div:not(#mySidepanel)').css("filter", "blur(0px)");
-             document.body.style.backgroundColor = "white"; 
-            $('#mySidepanel').toggleClass('opensidepanel');
-          }
-        });
+  $('.closebtn').click(function () {
+    clk++;
+    $('#tg').toggleClass('fa-times');
+    $('div:not(#mySidepanel)').css("filter", "blur(0px)");
+    document.body.style.backgroundColor = "white";
+    $('#mySidepanel').toggleClass('opensidepanel');
 
-      });
+  });
+
+  $(document).click(function (e) {
+    if (($(e.target).closest('#mySidepanel').length == 0) & ($('#mySidepanel').width() == 250)) {
+      if ($(e.target).closest('#tg').length != 0) {
+        togglebtn();
+      }
+      else {
+        clk++;
+        $('#tg').toggleClass('fa-times');
+        $('div:not(#mySidepanel)').css("filter", "blur(0px)");
+        document.body.style.backgroundColor = "white";
+        $('#mySidepanel').toggleClass('opensidepanel');
+      }
+    }
+  });
+
+});
