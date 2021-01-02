0bcb953023c3f2fa45d9df0e51e2334f39dbdd55
Nishith Patel <nishithpatel1511@users.noreply.github.com>
2021-01-02 15:22:33 +0530

    Gadget World

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,49 @@
-$('document').ready(function () {
-  $('#navbarToggler').blur(function (event) {
-    var sw = window.innerWidth;
-    if (sw < 768) {
-      $(".navbar-collapse").collapse('hide');
-    }
-  });
-});
+var clk = 0;
+      $(function () 
+      {
+          $('#tg').click(function () {
+          clk++;
+          $('#tg').toggleClass('fa-times');
+          if (clk % 2 == 0) {
+           $('div:not(#mySidepanel)').css("filter", "blur(0px)");
+           document.body.style.backgroundColor = "white"; 
+            $('#mySidepanel').css({ 'width': 0 });
+          }
+          else {
+            $('#mySidepanel').css({ 'width': 250 });
+           $('div:not(#mySidepanel)').css("filter", "blur(4px)");
+           document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+          }
+        });
+
+        $('.closebtn').click(function () {
+          clk++;
+          $('#tg').toggleClass('fa-times');
+          if (clk % 2 == 0) {
+            
+           $('div:not(#mySidepanel)').css("filter", "blur(0px)");
+            document.body.style.backgroundColor = "white"; 
+            $('#mySidepanel').css({ 'width': 0 });
+          }
+          else {
+            $('#mySidepanel').css({ 'width': 250 });
+            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+            
+           $('div:not(#mySidepanel)').css("filter", "blur(4px)");
+          }
+        });
+
+        // $('div:not(#mySidepanel)').click(function () {
+        // //  if (($('#mySidepanel').width()) == 250) {
+        // //     clk++;
+        // //     //  $('#tg').toggleClass('fa-times');
+        // //     $('div:not(#mySidepanel)').css("filter", "blur(0px)");
+        // //     document.body.style.backgroundColor = "white"; 
+        // //     $('#mySidepanel').css({ 'width': 0 });
+        // //   }
+        // clk++;
+        //   $('#tg').toggleClass('fa-times');
+        // });
+
+
+      });
