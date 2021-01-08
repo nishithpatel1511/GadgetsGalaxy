fabff17f283d8c0098b409a60ef81ab1f21bd7e7
nishithpatel1511 <nishithjpatel22@gmail.com>
2021-01-08 19:01:34 +0530

    Gadget WOrld

--- js/search/search.js
+++ js/search/search.js
@@ -1,58 +1,74 @@
-$(document).ready(function(){
-    $('#Search').keyup(function (){
+$(document).ready(function () {
 
-     //   alert("hello");
-     var s = $('#Search').val();
-      var exp = new RegExp(s,"i");
-      $('#result').html(''); 
-      $.ajax({
-        url: 'https://nishithpatel1511.github.io/Gadgetworld/js/search/mobile.json',
-        dataType:'json',
-        success : function(data){
-          $.each(data,function(key,value){
-            
-            if(value.name.search(exp) != -1 || value.company.search(exp) != -1)
-            {
-              $('#result').append('<li class="list-group-item"><img src = "'+value.image+'" class= "img-thumbnail" height=50px width=50px>'+value.name+' | <span class= "text-muted">'+value.company+'</span></li>'); 
-            } 
-            else
-            {
-              console.log(exp,value.name);
-            }
-            });
-        },
-        statusCode : {
-          404:function(){
-            alert("json file not found");
+  $('#Search').keyup(function () {
+    var s = $('#Search').val();
+    var exp = new RegExp(s, "i");
+    $('#result').html('');
+    $.ajax({
+      url: 'https://nishithpatel1511.github.io/Gadgetworld/js/search/mobile.json',
+      dataType: 'json',
+      success: function (data) {
+        $.each(data, function (key, value) {
+
+          if (value.name.search(exp) != -1 || value.company.search(exp) != -1) {
+            $('#result').append('<li class="list-group-item"><img src = "' + value.image + '" class= "img-thumbnail" height=50px width=50px>' + value.name + ' | <span class= "text-muted">' + value.company + '</span></li>');
           }
+        });
+      },
+      statusCode: {
+        404: function () {
+          alert("json file not found");
         }
-      });
+      }
     });
-
-    $('#Searchm').keyup(function (){
-  //   alert("hello");
-        var s = $('#Searchm').val();
-         var exp = new RegExp(s,"i");
-         $('#resultm').html(''); 
-         $.ajax({
-           url: 'https://nishithpatel1511.github.io/Gadgetworld/js/search/mobile.json',
-           dataType:'json',
-           success : function(data){
-             $.each(data,function(key,value){
-               
-               if(value.name.search(exp) != -1 || value.company.search(exp) != -1)
-               {
-                 $('#resultm').append('<li class="list-group-item"><img src = "'+value.image+'" class= "img-thumbnail" height=50px width=50px>'+value.name+' | <span class= "text-muted">'+value.company+'</span></li>');
-                
-               } 
-               });
-           },
-           statusCode : {
-             404:function(){
-               alert("json file not found");
-             }
-           }
-         });
-       });
-
   });
+
+  $('#Searcht').keyup(function () {
+    var s = $('#Searcht').val();
+    var exp = new RegExp(s, "i");
+    $('#resultt').html('');
+    $.ajax({
+      url: 'https://nishithpatel1511.github.io/Gadgetworld/js/search/mobile.json',
+      dataType: 'json',
+      success: function (data) {
+        $.each(data, function (key, value) {
+
+          if (value.name.search(exp) != -1 || value.company.search(exp) != -1) {
+            $('#resultt').append('<li class="list-group-item"><img src = "' + value.image + '" class= "img-thumbnail" height=50px width=50px>' + value.name + ' | <span class= "text-muted">' + value.company + '</span></li>');
+
+          }
+        });
+      },
+      statusCode: {
+        404: function () {
+          alert("json file not found");
+        }
+      }
+    });
+  });
+
+  $('#Searchm').keyup(function () {
+    var s = $('#Searchm').val();
+    var exp = new RegExp(s, "i");
+    $('#resultm').html('');
+    $.ajax({
+      url: 'https://nishithpatel1511.github.io/Gadgetworld/js/search/mobile.json',
+      dataType: 'json',
+      success: function (data) {
+        $.each(data, function (key, value) {
+
+          if (value.name.search(exp) != -1 || value.company.search(exp) != -1) {
+            $('#resultm').append('<li class="list-group-item"><img src = "' + value.image + '" class= "img-thumbnail" height=50px width=50px>' + value.name + ' | <span class= "text-muted">' + value.company + '</span></li>');
+
+          }
+        });
+      },
+      statusCode: {
+        404: function () {
+          alert("json file not found");
+        }
+      }
+    });
+  });
+
+});
