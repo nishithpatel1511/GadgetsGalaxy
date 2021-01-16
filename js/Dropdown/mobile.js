cbb6ee54ece0a14ff6034728ffa326624cbcb498
nishithpatel1511 <nishithjpatel22@gmail.com>
2021-01-16 14:05:00 +0530

    Gadgets Galaxy

--- js/Dropdown/mobile.js
+++ js/Dropdown/mobile.js
@@ -14,13 +14,13 @@ $(function () {
         $.each(value, function (key, val) {
         
           row = '#mrow-' + r;
-          $(row).append('<div class="col-3 megamenu-col" id="mcol-' + l + '" style="text-align: center;"></div>');
-          col = '#mcol-' + l;
-          $(col).append('<h3 id="megamenu-title">' + key + '</h3><ul class="list-unstyled" id="megamenu-list-' + l + '"></ul>');
-          // list = '#megamenu-list-' + l;
-          // $.each(val, function (k, v) {
-          //    $(list).append('<li><a href="' + v.link + '">' + v.name +'</a></li>');
-          //   });
+          $(row).append('<div class="col-3 megamenu-col" id="'+r+'-mcol-' + l + '" style="text-align: center;"></div>');
+          col = '#'+r+'-mcol-' + l;
+          $(col).append('<h3 id="megamenu-title">' + key + '</h3><ul class="list-unstyled megamenu-list" id="'+r+'-list-' + l + '"></ul>');
+          list = '#'+r+'-list-' + l;
+          $.each(val, function (k, v) {
+             $(list).append('<li><a href="' + v.link + '">' + v.name +'</a></li>');
+            });
            l++;
         });
          r++;
