77519f3102f07a5459c54566514b01586112a7a8
nishithpatel1511 <nishithjpatel22@gmail.com>
2021-01-16 15:18:55 +0530

    Gadgets Galaxy

--- js/Dropdown/mobile.js
+++ js/Dropdown/mobile.js
@@ -1,5 +1,6 @@
 r = 1;
 l = 1;
+n=0;
 $(function () {
   $.ajax({
     url: "https://nishithpatel1511.github.io/GadgetsGalaxy/js/Dropdown/D-mobile.json",
@@ -10,11 +11,14 @@ $(function () {
 
 
         $('#megamenu-temp').append('<div class="row megamenu-row" id="mrow-' + r + '"></div>');
-
+        $.each(value, function (key, val) {
+          n++;
+        });
+        n=12/n;
         $.each(value, function (key, val) {
         
           row = '#mrow-' + r;
-          $(row).append('<div class="col-3 megamenu-col" id="'+r+'-mcol-' + l + '" style="text-align: center;"></div>');
+          $(row).append('<div class="col-'+n+' megamenu-col" id="'+r+'-mcol-' + l + '" style="text-align: center;"></div>');
           col = '#'+r+'-mcol-' + l;
           $(col).append('<h3 id="megamenu-title">' + key + '</h3><ul class="list-unstyled megamenu-list" id="'+r+'-list-' + l + '"></ul>');
           list = '#'+r+'-list-' + l;
@@ -24,6 +28,7 @@ $(function () {
            l++;
         });
          r++;
+         n=0;
          l=1;
       });
     }
