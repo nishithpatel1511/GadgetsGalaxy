3f94e9f0ee65b3ab7cb12c854de6ab78d1667efc
nishithpatel1511 <nishithjpatel22@gmail.com>
2021-01-16 15:37:36 +0530

    Gadgets Galaxy

--- js/Dropdown/mobile.js
+++ js/Dropdown/mobile.js
@@ -1,6 +1,6 @@
 r = 1;
 l = 1;
-n=0;
+n = 0;
 $(function () {
   $.ajax({
     url: "https://nishithpatel1511.github.io/GadgetsGalaxy/js/Dropdown/D-mobile.json",
@@ -11,25 +11,33 @@ $(function () {
 
 
         $('#megamenu-temp').append('<div class="row megamenu-row" id="mrow-' + r + '"></div>');
+
         $.each(value, function (key, val) {
           n++;
         });
-        n=12/n;
+
+
         $.each(value, function (key, val) {
-        
-          row = '#mrow-' + r;
-          $(row).append('<div class="col-'+n+' megamenu-col" id="'+r+'-mcol-' + l + '" style="text-align: center;"></div>');
-          col = '#'+r+'-mcol-' + l;
-          $(col).append('<h3 id="megamenu-title">' + key + '</h3><ul class="list-unstyled megamenu-list" id="'+r+'-list-' + l + '"></ul>');
-          list = '#'+r+'-list-' + l;
-          $.each(val, function (k, v) {
-             $(list).append('<li><a href="' + v.link + '">' + v.name +'</a></li>');
+
+
+          if (n == 2 & (l == 1 || l == 4)) {
+            $(row).append('<div class="col-3 megamenu-col" id="' + r + '-mcol-0" style="text-align: center;"></div>');
+          }
+          else {
+            row = '#mrow-' + r;
+            $(row).append('<div class="col-3 megamenu-col" id="' + r + '-mcol-' + l + '" style="text-align: center;"></div>');
+            col = '#' + r + '-mcol-' + l;
+            $(col).append('<h3 id="megamenu-title">' + key + '</h3><ul class="list-unstyled megamenu-list" id="' + r + '-list-' + l + '"></ul>');
+            list = '#' + r + '-list-' + l;
+            $.each(val, function (k, v) {
+              $(list).append('<li><a href="' + v.link + '">' + v.name + '</a></li>');
             });
-           l++;
+            l++;
+          }
         });
-         r++;
-         n=0;
-         l=1;
+        r++;
+        n = 0;
+        l = 1;
       });
     }
   });
